4af91b82dff74c9f6f4e7736ebd5b8e725cd7627
Ivana Sovilj <isovilj11@outlook.com>
2020-04-01 13:33:11 +0200

    add models into API

---
+++ OpenSourceSoftwareDevelopment.Museum.API/Models/CreateExhibitionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenSourceSoftwareDevelopment.Museum.API.Models
+{
+    public class CreateExhibitionModel
+    {
+        public int Id { get; set; }
+        public string ExcibitionName { get; set; }
+        public int AuditoriumId { get; set; }
+        public string TypeOfExhibition { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
